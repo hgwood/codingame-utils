9ab1d1bacaf6cb2a801c0cb9f71de2e5125f3e88
Hugo Wood <hx4.5@free.fr>
2016-11-29 00:43:08 +0100

    feat(fantastic-bits): parsing

---
+++ fantastic-bits/parse.js
@@ -0,0 +1,37 @@
+const parse = require('../parse')
+
+module.exports = {turnInfo}
+
+function turnInfo (readline) {
+  return parse.entities(readline, 'id', 'type', 'x', 'y', 'vx', 'vy', 'state')
+    .map(entity => Object.assign(entity, {
+      position: {x: entity.x, y: entity.y},
+      velocity: {x: entity.vx, y: entity.vy},
+      hasSnaffle: entity.state === 1,
+      isWizard: entity.type === 'WIZARD',
+      isOpponent: entity.type === 'OPPONENT_WIZARD',
+      isSnaffle: entity.type === 'SNAFFLE',
+      isBludger: entity.type === 'BLUDGER'
+    }))
+    .map(entity => Object.assign(entity, {
+      isPlayer: entity.isWizard || entity.isOpponent,
+      isBall: entity.isSnaffle || entity.isBludger
+    }))
+    .reduce((entities, entity) => {
+      entities.all.push(entity)
+      if (entity.isPlayer) entities.players.push(entity)
+      if (entity.isWizard) entities.wizards.push(entity)
+      if (entity.isOpponent) entities.opponents.push(entity)
+      if (entity.isBall) entities.balls.push(entity)
+      if (entity.isSnaffle) entities.snaffles.push(entity)
+      if (entity.isBludger) entities.bludgers.push(entity)
+    }, {
+      all: [],
+      players: [],
+      wizards: [],
+      opponents: [],
+      balls: [],
+      snaffles: [],
+      bludgers: []
+    })
+}
