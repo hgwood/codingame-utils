759fc0cb50e69b93ee11a5c492a3383ed644521d
Hugo Wood <hx4.5@free.fr>
2016-11-29 21:14:41 +0100

    feat(fantastic-bits): low level movement functions

--- fantastic-bits/move.js
+++ fantastic-bits/move.js
@@ -1,6 +1,12 @@
 const {normalize, between, scale, add} = require('../vector')
 
-module.exports = {step}
+module.exports = {
+  step,
+  nextVelocity,
+  nextPosition,
+  nextMoveVelocity,
+  nextMove
+}
 
 /**
  * @typedef Entity
@@ -24,9 +30,44 @@ module.exports = {step}
  * @param {Entity} target entity that the entity is moving towards
  */
 function step (entity, target) {
-  const velocityDelta = scale(scale(normalize(between(entity, target)), entity.thrust), 1 / entity.mass)
-  const movementVelocity = add(entity.velocity, velocityDelta)
-  const newPosition = add(entity.position, movementVelocity)
-  const newVelocity = scale(movementVelocity, entity.friction)
-  return Object.assign({}, entity, {position: newPosition, velocity: newVelocity})
+  return Object.assign({}, entity, {
+    position: nextPosition(entity, target),
+    velocity: nextVelocity(entity, target)
+  })
+}
+
+/**
+ * @returns {Vector}
+ * @param {Entity} entity entity for which to simulate the movement
+ * @param {Entity} target entity that the entity is moving towards
+ */
+function nextVelocity (entity, target) {
+  return scale(nextMoveVelocity(entity, target), entity.friction)
+}
+
+/**
+ * @returns {Vector}
+ * @param {Entity} entity entity for which to simulate the movement
+ * @param {Entity} target entity that the entity is moving towards
+ */
+function nextPosition (entity, target) {
+  return add(entity.position, nextMoveVelocity(entity, target))
+}
+
+/**
+ * @returns {Vector}
+ * @param {Entity} entity entity for which to simulate the movement
+ * @param {Entity} target entity that the entity is moving towards
+ */
+function nextMoveVelocity (entity, target) {
+  return add(entity.velocity, nextMove(entity, target))
+}
+
+/**
+ * @returns {Vector}
+ * @param {Entity} entity entity for which to simulate the movement
+ * @param {Entity} target entity that the entity is moving towards
+ */
+function nextMove (entity, target) {
+  return scale(scale(normalize(between(entity, target)), entity.thrust), 1 / entity.mass)
 }
