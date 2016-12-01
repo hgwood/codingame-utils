08e27b4fd8459b859c72e59e46dd754309ab4276
Hugo Wood <hx4.5@free.fr>
2016-12-01 01:16:55 +0100

    docs: docs for fantastic-bits/arena

--- src/fantastic-bits/arena.js
+++ src/fantastic-bits/arena.js
@@ -1,5 +1,18 @@
+/**
+ * @module fantastic-bits/arena
+ */
 module.exports = {
+  /**
+   * Coordinates of the top left corner of the arena.
+   */
   topLeft: {x: 0, y: 0},
+  /**
+   * Coordinates of the bottom right corner of the arena.
+   */
   bottomRight: {x: 16000, y: 7500},
+  /**
+   * @returns the center of the goal you should put the ball in
+   * @param {number} teamId
+   */
   target: teamId => [{x: 16000, y: 3750}, {x: 0, y: 3750}][teamId]
 }
